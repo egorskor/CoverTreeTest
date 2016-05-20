70374b40873815759414e0ae3a67f066bcdb1dc1
egorskor <egorskor1995@mail.ru>
2016-05-20 15:28:58 +0300

    second commit

--- CoverTreeTest/CoverTreeTest/Main.cpp
+++ CoverTreeTest/CoverTreeTest/Main.cpp
@@ -4,12 +4,14 @@
 #include <map>
 #include <vector>
 #include <set>
+#include <ctime> 
 using namespace std;
 
-const int MAX_LEVEL = 10;
-const int MIN_LEVEL = -10;
+const double MAX_LEVEL = ceilf(log(INT_MAX) / log(2.0));
+const double MIN_LEVEL = ceilf(log(FLT_MIN) / log(2.0));
 
 using std::string;
+
 class Object
 {
 	double value;
@@ -19,7 +21,7 @@ public:
 		value = _value;
 	}
 
-	double get_value()
+	double get_value() const
 	{
 		return value;
 	}
@@ -30,23 +32,43 @@ template <typename dist_t>
 class KNNQuery {
 	Object* query;
 	int k;
-	vector<Object*>* objectVector;
+	int DistCount = 0;
+	vector<const Object*>* objectVector;
 	vector<dist_t>* distVector;
 public:
 	KNNQuery(int _k, Object* _query)
 	{
 		k = _k;
 		query = _query;
-		objectVector = new vector<Object*>;
+		objectVector = new vector<const Object*>;
+		distVector = new vector<dist_t>;
 	}
 
 	int get_k()
 	{
 		return k;
 	}
-
-	dist_t DistanceObjLeft(Object* object)
+	vector<double> get_Points()
 	{
+		vector<double> toReturn;
+		for (int i = 0; i < objectVector->size(); i++)
+		{
+			toReturn.push_back((*objectVector)[i]->get_value());
+		}
+		return toReturn;
+	}
+
+	vector<const Object*> get_Objects()
+	{
+		return *objectVector;
+	}
+	vector<dist_t> get_Dists()
+	{
+		return *distVector;
+	}
+	dist_t DistanceObjLeft(const Object* object)
+	{
+		DistCount++;
 		return abs(query->get_value() - object->get_value());
 	}
 
@@ -59,7 +81,7 @@ public:
 	{
 		objectVector->push_back(object);
 	}
-	void CheckAndAddToResult(const vector<Object*>& bucket) 
+	void CheckAndAddToResult(const vector<Object*>& bucket)
 	{
 		objectVector->insert(objectVector->end(), bucket.begin(), bucket.end());
 	}
@@ -70,9 +92,9 @@ class Space
 public:
 	Space()
 	{}
-	int IndexTimeDistance(Object* a, Object* b)
+	dist_t IndexTimeDistance(const Object* obj1, const Object* obj2)
 	{
-		return abs(a->get_value() - b->get_value());
+		return abs(obj1->get_value() - obj2->get_value());
 	}
 };
 template <typename dist_t>
@@ -84,20 +106,19 @@ private:
 	private:
 		map<int, vector<CoverTreeNode*>> children;
 		//Object* object;
-		vector<Object*>* objectVector;
+		vector<const Object*> objectVector;
 	public:
-		CoverTreeNode(Object* _object)
+		CoverTreeNode(const Object* _object)
 		{
 			//object = _object;
-			objectVector = new vector<Object*>;
-			objectVector->push_back(_object);
+			//vector<const Object*>objectVector;
+			objectVector.push_back(_object);
 		}
-		Object* get_object()
+		const Object* get_object()
 		{
-
-			return (*objectVector)[0];
+			return objectVector[0];
 		}
-		vector<Object*>* get_all()
+		vector<const Object*> get_all()
 		{
 			return objectVector;
 		}
@@ -112,18 +133,17 @@ private:
 		}
 		void insert_child(int level, CoverTreeNode* child)
 		{
-			
 			children[level].push_back(child);
 		}
 
 		void insert_clone(CoverTreeNode* clone)
 		{
-			objectVector->push_back(clone->get_object());
+			objectVector.push_back(clone->get_object());
 		}
 	};//CoverTreeNode
 	CoverTreeNode* root;
 public:
-	CoverTreeMethod( Space<dist_t>* space,
+	CoverTreeMethod(Space<dist_t>* space,
 		const vector<Object*> data)
 	{
 		if (root == NULL)
@@ -137,19 +157,19 @@ public:
 		}
 	}
 
-	bool insert( Space<dist_t>* space, CoverTreeNode* point, vector<CoverTreeNode*>& level_points, int level)
+	bool insert(Space<dist_t>* space, CoverTreeNode* point, vector<CoverTreeNode*>& level_points, int level)
 	{
-		vector<CoverTreeNode*> children =  level_points;
+		vector<CoverTreeNode*> children = level_points;
 		for (int i = 0; i < level_points.size(); i++)
 		{
-			vector<CoverTreeNode*> tmp=level_points[i]->get_children(level);
+			vector<CoverTreeNode*> tmp = level_points[i]->get_children(level);
 			children.insert(children.end(), tmp.begin(), tmp.end());
 		}
 		vector<CoverTreeNode*> parents;
 		for (int i = 0; i < children.size(); i++)
 		{
-			float check = pow(2, level);
-			if (!(space->IndexTimeDistance(point->get_object(), children[i]->get_object())>=check))//...			
+			double check = pow(2, level);
+			if (!(space->IndexTimeDistance(point->get_object(), children[i]->get_object()) >= check))//...			
 			{
 				if (space->IndexTimeDistance(point->get_object(), children[i]->get_object()) == 0)
 				{
@@ -185,20 +205,21 @@ public:
 	void Search(KNNQuery<dist_t>* query)
 	{
 		int k = query->get_k();
-		set<pair<dist_t,CoverTreeNode*>> answer = { make_pair(query->DistanceObjLeft(root->get_object()), root)};
-		vector<CoverTreeNode*> level_points = { root };
-		dist_t minDist = query->DistanceObjLeft(root->get_object());//дл€ работы алгоритма
-		dist_t maxDist = minDist;//дл€ составлени€ ответа
+		vector<pair<dist_t, CoverTreeNode*>> level_points = { make_pair(query->DistanceObjLeft(root->get_object()), root) };
+		set<pair<dist_t, CoverTreeNode*>>answer;
+		answer.insert(level_points[0]);
+		dist_t minDist = level_points[0].first;//дл€ работы алгоритма
+		dist_t maxDist = level_points[0].first;//дл€ составлени€ ответа
 		for (int level = MAX_LEVEL; level >= MIN_LEVEL; level--)
 		{
 			for (int j = 0; j < level_points.size(); j++)
 			{
-				vector<CoverTreeNode*> children = level_points[j]->get_children(level);
+				vector<CoverTreeNode*> children = level_points[j].second->get_children(level);
 				for (vector<CoverTreeNode*>::iterator i = children.begin(); i != children.end(); i++)
 				{
 					dist_t dist = query->DistanceObjLeft((*i)->get_object());
 					if (dist <= minDist)
-					{		
+					{
 						minDist = dist;
 					}
 					if (dist < maxDist || answer.size() < k)
@@ -210,26 +231,29 @@ public:
 						if (answer.size() > k) answer.erase(--answer.end());
 						maxDist = (--answer.end())->first;
 					}
-					level_points.push_back(*i);
+					level_points.push_back(make_pair(dist, *i));
 				}
 			}
 			dist_t check = pow(2, level) + minDist;
 			int size = level_points.size();
-			for (int i = 0; i<size; i++) 
+			for (int i = 0; i<size; i++)
 			{
-			   if (query->DistanceObjLeft(level_points[i]->get_object()) > check)//дистанци€ считаетс€ второй раз. ћожетстоило еЄ сохранить?
-			   {
-				 level_points.erase(level_points.begin() + i);
-				 size--; i--;
-			   }
-			}  
+				if (level_points[i].first > check)//дистанци€ считаетс€ второй раз. ћожетстоило еЄ сохранить?
+				{
+					level_points.erase(level_points.begin() + i);
+					size--; i--;
+				}
+			}
 		}
 		//set<pair<dist_t, CoverTreeNode*>>::iterator it
 		for (set<pair<dist_t, CoverTreeNode*>>::iterator it = answer.begin(); it != answer.end(); it++)//....
 		{
 			//CoverTreeNode* a= answer[it].second;
-			vector<Object*>* objectVector=it->second->get_all();
-			query->CheckAndAddToResult(*objectVector);
+			vector<const Object*> objectVector = it->second->get_all();
+			for (int i = 0; i < objectVector.size(); i++)
+			{
+				query->CheckAndAddToResult(it->first, objectVector[i]);
+			}
 		}
 	}
 
@@ -239,13 +263,85 @@ public:
 	}*/
 };//CoverTree
 
+void test(int N, int K)//N is amount of objects in tree; K is required amound of neighbours
+{
+	cout << "Test with " << N << " objects begins" << endl;
+	Object* search_object = new Object(rand());
+	KNNQuery<double>* query = new KNNQuery<double>(K, search_object);//второй аргумент точка, к которой ищем ближайших соседей; первый аргумент - их количество
+	Space<double>* fakespace = new Space<double>();
+	vector<Object*> data;
+	multiset<pair<double, const Object*>>data_set;//<dist,Object>
+	multiset<pair<double, const Object*>>check;
+	//vector<Object*> data = {new Object(1), new Object(5), new Object(6),  new Object(6), new Object(7), new Object(46), new Object(3), new Object(2) };
+	for (int i = 0; i < N; i++)
+	{
+		Object* r = new Object(rand());
+		data.push_back(r);
+		data_set.insert(make_pair(fakespace->IndexTimeDistance(r, search_object), r));
+	}
+	multiset<pair<double, const Object*>>::iterator it = data_set.begin();
+	for (int i = 0; i < query->get_k(); i++)
+	{
+		if (it == data_set.end())
+		{
+			cout << "ERROR:K is bigger than data.size()" << endl;
+			break;
+		}
+		check.insert(*it);
+		it++;
+	}
+	cout << "Data set created..." << endl;
+
+	unsigned int start_tree_creation_time = clock();
+	CoverTreeMethod<double> a(fakespace, data);
+	unsigned int end_tree_creation_time = clock();
+	cout << "Tree creation time: " << end_tree_creation_time - start_tree_creation_time << endl;
+
+	unsigned int start_search_time = clock();
+	a.Search(query);
+	unsigned int end_search_time = clock();
+	cout << "Search time: " << end_search_time - start_search_time << endl;
+
+	vector<const Object*> result = query->get_Objects();
+	vector<double> distResult = query->get_Dists();
+	int right_answer_count = 0;
+	int i = 0;
+	//for (multiset<pair<double, Object*>>::iterator it = check.begin(); i < query->get_k(); it++)//тк элементы могут идти в разном пор€дке - не идеально
+	//{
+	// if (i >= result.size())
+	// {
+	//	 cout << "search returned less objects than k" << endl;
+	//	 break;
+	// }
+	// if (it->second == result[i])
+	// {
+	//	 right_answer_count++;
+	// }
+	// i++;
+	//}
+	//vector<const Object*>::iterator it = result.begin(); it != result.end(); it++
+	for (i; i < query->get_k(); i++)
+	{
+		if (i >= result.size())
+		{
+			cout << "ERROR:Search returned less objects than k" << endl;
+			break;
+		}
+		multiset<pair<double, const Object*>>::iterator it2 = check.find(make_pair(distResult[i], result[i]));
+		if (it2 != check.end())
+		{
+			right_answer_count++;
+		}
+	}
+	cout << "Right neighbours " << right_answer_count << endl;
+	cout << "From k=" << query->get_k() << endl;
+	cout << "Percentage of right answers: " << (float)right_answer_count / i << endl;
+}
 int main()
 {
-	 KNNQuery<int>* query=new KNNQuery<int> (2, new Object(5));//второй аргумент точка, к которой ищем ближайших соседей; первый аргумент - их количество
-	 Space<int>* fakespace = new Space<int>();
-	 vector<Object*> data = {new Object(1), new Object(5), new Object(6),  new Object(6), new Object(7), new Object(46), new Object(3), new Object(2) };
-	 CoverTreeMethod<int> a(fakespace, data);
-	 a.Search(query);
-	 CoverTreeMethod<int> b =  a;
-	
+	test(100, 30);//first argument is amount of objects in tree; second is required amound of neighbours
+	test(10000, 30);
+	test(100000, 30);
+	system("pause");
+
 }
